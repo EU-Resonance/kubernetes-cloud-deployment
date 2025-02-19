24ff1f8da9d61d9514b8091caf0648d20fb31e22
Kari Kolehmainen <kari.kolehmainen@vtt.fi>
2025-02-19 11:46:35 +0000

    fixed legacy url

--- restart.sh
+++ restart.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-microk8s stop
-snap remove microk8s
-iptables -F
-iptables -X
-snap install microk8s --classic --channel=1.30/stable
-microk8s stop
+USERNAME=$USER
+EXTERNAL_IP=$(hostname -I | awk '{print $1}')
+#EXTERNAL_IP=$(curl -s https://api64.ipify.org)
+sudo snap remove --purge microk8s
+sudo iptables -F
+sudo iptables -X
+sudo snap install microk8s --classic --channel=1.32/stable
+sudo usermod -a -G microk8s $USERNAME
 microk8s start
 microk8s status --wait-ready
 microk8s enable community
 microk8s enable dns
-microk8s enable hostpath-storage
-microk8s enable istio
+microk8s enable metallb:$EXTERNAL_IP-$EXTERNAL_IP
+sudo microk8s enable hostpath-storage
+sudo microk8s enable istio
 microk8s status
 microk8s stop
-echo "--service-node-port-range=1-65535" >> /var/snap/microk8s/current/args/kube-apiserver
+sudo echo "--service-node-port-range=1-65535" >> /var/snap/microk8s/current/args/kube-apiserver
 microk8s start
 microk8s status --wait-ready
 microk8s.helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
@@ -32,7 +35,10 @@ microk8s kubectl label namespace default istio-injection=enabled
 # Build cluster
 #
 microk8s.kubectl -v=0 kustomize deployment/mesh-infra/ | microk8s kubectl -v=0 apply -f -
-microk8s.kubectl create -n istio-system secret tls istio-gw-cert --key=/etc/cert/server.key --cert=/etc/cert/fullchain.crt
+# Above install does not go thru all that well so we need to run it again but first wait for a while to let the cluster to absorb above
+sleep 10
+microk8s.kubectl -v=0 kustomize deployment/mesh-infra/ | microk8s kubectl -v=0 apply -f -
+sudo microk8s.kubectl create -n istio-system secret tls istio-gw-cert --key=/etc/cert/server.key --cert=/etc/cert/fullchain.crt
 #
 # Create sealed secrets from templates
 #
@@ -40,6 +46,9 @@ cd deployment/mesh-infra/security/secrets
 kubeseal -o yaml < templates/keycloak-builtin-admin.yaml > keycloak-builtin-admin.yaml
 kubeseal -o yaml < templates/argocd.yaml > argocd.yaml
 kubeseal -o yaml < templates/oidc-clients.yaml > oidc-clients.yaml
+microk8s.kubectl apply -f keycloak-builtin-admin.yaml
+microk8s.kubectl apply -f argocd.yaml
+microk8s.kubectl apply -f oidc-clients.yaml
 cd ../../../../
 
 microk8s.kubectl get secrets --all-namespaces
